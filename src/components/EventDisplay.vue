09e240d79650a56a0ef5c7e020ba470fcc37eb5d
Hashan Hirosh <hashan@thinkcube.com>
2018-08-17 16:22:06 +0530

    Before route

---
+++ src/components/EventDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<table class="table table-hover">
+		    <tbody>
+		      <tr v-for="(item, index) in restList">
+		        <td>{{ index }}</td>
+		        <td>{{ item }}</td>
+		      </tr>
+		    </tbody>
+		</table>
+	</div>
+</template>
+
+<script>
+	export default{
+
+		props:['restList']
+
+	}
+</script>
